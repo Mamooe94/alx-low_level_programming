a272ff84594b01a8c4239365f413a7a25c4981ff
Mamooe94 <tsepangmamooe381@gmail.com>
2023-03-04 14:47:45 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,7 @@ void puts2(char *str)
 	int longi = 0;
 	int t = 0;
 	char *r = str;
-
-	int 0;
+	int o;
 
 	while (*r != '\0')
 	{
@@ -21,10 +20,10 @@ void puts2(char *str)
 	t = longi - 1;
 	for (o = 0 ; o <= t ; o++)
 	{
-		if (o % 2 == 0)
-		{
-			putchar(str[o]);
-		}
+	if (o % 2 == 0)
+	{
+	putchar(str[o]);
+	}
 	}
 	_putchar('\n')
 }
