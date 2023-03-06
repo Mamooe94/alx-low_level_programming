db955103ffdb83a76c8c85dfdcd9c52fd663b681
Mamooe94 <tsepangmamooe381@gmail.com>
2023-03-06 08:18:47 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr -a function that locates a file in a string
+ * @s: input
+ * @c: input
+ * Return: 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+	}
+	return (0);
+}
